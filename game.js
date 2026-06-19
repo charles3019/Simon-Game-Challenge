cd0fca6c8d639dec50bb4cb338d691644d597228
Charles Agyemang <kwesi.c96@gmail.com>
2026-06-19 12:55:34 +0100

    Add jQuery button flash animation and sound playback

--- game.js
+++ game.js
@@ -5,5 +5,14 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-  alert("game.js loaded");
+
+  // Step 1: Use jQuery to select the button with the same id as randomChosenColour
+  var chosenButton = $("#" + randomChosenColour);
+
+  // Step 2: Animate a flash effect to the button
+  chosenButton.fadeIn(100).fadeOut(100).fadeIn(100);
+
+  // Step 3: Play the sound for the button colour
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
